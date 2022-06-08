85d77ecd429a4fe22b161956c32cffb9a7bfa287
Adrian Wong <adrianwjw@gmail.com>
2022-06-08 16:04:43 +1000

    Read .html files

--- src/munchkin.m
+++ src/munchkin.m
@@ -15,9 +15,38 @@
 
 :- implementation.
 
+:- import_module list.
+:- import_module string.
+
 :- import_module dom.
 
 %------------------------------------------------------------------------------%
 
 main(!IO) :-
-    io.write_string("Hello, world!\n", !IO).
+    io.command_line_arguments(Args, !IO),
+    ( if
+        Args = [InputFile],
+        string.suffix(InputFile, file_extension)
+    then
+        io.read_named_file_as_string(InputFile, ReadRes, !IO),
+        (
+            ReadRes = ok(Input),
+            io.write_string(Input, !IO)
+        ;
+            ReadRes = error(Err),
+            io.format(io.stderr_stream, "I/O error: %s.\n",
+                [s(io.error_message(Err))], !IO),
+            io.set_exit_status(1, !IO)
+        )
+    else
+        io.progname_base("munchkin", Prog, !IO),
+        io.format(io.stderr_stream, "Usage: %s file%s.\n",
+            [s(Prog), s(file_extension)], !IO),
+            io.set_exit_status(1, !IO)
+    ).
+
+%------------------------------------------------------------------------------%
+
+:- func file_extension = string.
+
+file_extension = ".html".
